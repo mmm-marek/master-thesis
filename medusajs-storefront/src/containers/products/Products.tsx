106fbaf0eebbe03587ea74b898d28c00c07f72f8
Marek Matusica <marek.matusica@goodrequest.com>
2024-05-02 13:33:17 +0200

    Replace props drilling with state dehydration

--- medusajs-storefront/src/containers/products/Products.tsx
+++ medusajs-storefront/src/containers/products/Products.tsx
@@ -1,20 +1,28 @@
-import { PricedProduct } from '@medusajs/medusa/dist/types/pricing'
+import Medusa from '@medusajs/medusa-js'
+import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
+
+import { QUERY_KEYS } from '@/utils/enums'
 
 import * as SC from './ProductsStyles'
 
-type ProductsProps = {
-	products: PricedProduct[]
-}
+const Products = () => {
+	const { data } = useQuery([QUERY_KEYS.API_GET_PRODUCTS], async () => {
+		const medusa = new Medusa({ baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL, maxRetries: 3 })
+		const products = await medusa.products.list()
 
-const Products = ({ products }: ProductsProps) => {
+		return products.products
+	})
+
+	const products = data ?? []
+
 	return (
 		<SC.ProductsGrid>
 			{products.map((product) => (
 				<SC.Product key={product.id}>
 					{product.images?.at(0) && (
 						<SC.ImageWrapper>
-							<Image src={product.images.at(0)!.url} alt={product.title ?? 'Product'} width={50} height={50} unoptimized objectFit='contain' />
+							<Image src={product.images.at(0)!.url} alt={product.title ?? 'Product'} width={50} height={50} unoptimized />
 						</SC.ImageWrapper>
 					)}
 					<SC.ProductTextContent>
--- medusajs-storefront/src/pages/index.tsx
+++ medusajs-storefront/src/pages/index.tsx
@@ -1,26 +1,36 @@
 import Medusa from '@medusajs/medusa-js'
-import { InferGetStaticPropsType } from 'next'
+import { QueryClient, dehydrate } from '@tanstack/react-query'
 import { ReactElement } from 'react'
 
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary'
 import Products from '@/containers/products/Products'
 import MainLayout from '@/layouts/MainLayout/MainLayout'
+import { QUERY_KEYS } from '@/utils/enums'
 import { getLocales } from '@/utils/locales'
 
 export const getStaticProps = async ({ locale }: { locale?: string }) => {
-	const medusa = new Medusa({ baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL, maxRetries: 3 })
-	const products = await medusa.products.list()
+	const queryClient = new QueryClient()
+
+	await queryClient.prefetchQuery({
+		queryKey: [QUERY_KEYS.API_GET_PRODUCTS],
+		queryFn: async () => {
+			const medusa = new Medusa({ baseUrl: process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL, maxRetries: 3 })
+			const products = await medusa.products.list()
+
+			return products.products
+		}
+	})
 
 	return {
 		props: {
 			locales: await getLocales(locale),
-			products: products.products
+			dehydratedState: dehydrate(queryClient)
 		}
 	}
 }
 
-const DashboardPage = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
-	return <Products products={products} />
+const DashboardPage = () => {
+	return <Products />
 }
 
 DashboardPage.getLayout = function getLayout(page: ReactElement) {
