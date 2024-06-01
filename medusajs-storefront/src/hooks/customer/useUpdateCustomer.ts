727053f8ddb50d89e17a7dfd4cf042071e078114
Marek Matušica <marek.matusica@goodrequest.com>
2024-06-01 17:37:25 +0200

    Add useUpdateCustomer hook

---
+++ medusajs-storefront/src/hooks/customer/useUpdateCustomer.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { QUERY_KEYS } from '@/utils/enums'
+import { medusa } from '@/utils/medusaHelpers'
+
+type UpdateCustomerData = Parameters<typeof medusa.customers.update>[0]
+
+const useUpdateCustomer = () => {
+	const queryClient = useQueryClient()
+
+	return useMutation({
+		mutationFn: async (data: UpdateCustomerData) => {
+			const { customer } = await medusa.customers.update(data)
+			return { customer }
+		},
+		onSuccess: (customer) => {
+			queryClient.setQueryData([QUERY_KEYS.API_GET_CUSTOMER_PROFILE], () => customer)
+		}
+	})
+}
+
+export default useUpdateCustomer
