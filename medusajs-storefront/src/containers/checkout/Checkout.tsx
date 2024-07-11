ab1b670286aebb2faa0b7c1820e5034b935d528a
Marek Matušica <marek.matusica@goodrequest.com>
2024-07-11 17:17:03 +0200

    Add initial checkout accordion

--- medusajs-storefront/src/containers/checkout/Checkout.tsx
+++ medusajs-storefront/src/containers/checkout/Checkout.tsx
@@ -1,9 +1,27 @@
-const Checkout = () => {
-	return (
-		<div>
-			<h1>Checkout</h1>
-		</div>
-	)
-}
+import { Collapse } from 'antd'
+
+import type { CollapseProps } from 'antd'
+
+const items: CollapseProps['items'] = [
+	{
+		key: '1',
+		label: 'Shipping address',
+		children: <p>Select shipping address</p>
+	},
+	{
+		key: '2',
+		label: 'Billing address',
+		children: <p>Select billing address</p>,
+		collapsible: 'disabled'
+	},
+	{
+		key: '3',
+		label: 'Payment details',
+		children: <p>Payment details</p>,
+		collapsible: 'disabled'
+	}
+]
+
+const Checkout = () => <Collapse accordion items={items} />
 
 export default Checkout
