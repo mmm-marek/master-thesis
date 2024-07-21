ac7fbe6cc4c1e9ef7c24050776517af51473ebb9
Marek Matušica <marek.matusica@goodrequest.com>
2024-07-21 20:40:31 +0200

    Style mobile hero banner

--- medusajs-storefront/src/containers/landing/components/HeroBanner/HeroBannerStyles.ts
+++ medusajs-storefront/src/containers/landing/components/HeroBanner/HeroBannerStyles.ts
@@ -1,40 +1,57 @@
 import Image from 'next/image'
 import styled, { css } from 'styled-components'
 
-import { headingXxlBold, textMdMedium } from '@/styles/helpers'
+import { breakpoints, headingXxlBold, textMdMedium, textXlSemibold } from '@/styles/helpers'
 
 export const Container = styled.div`
-	position: relative;
-	border-radius: 24px;
-	width: 100%;
-	height: 400px;
-	overflow: hidden;
-
-	&::after {
-		position: absolute;
-		top: 0;
-		left: 0;
-		opacity: 0.9;
-		z-index: 1;
-		background: linear-gradient(to right, rgb(255 255 255 / 100%), rgb(0 0 0 / 0%));
+	${({ theme }) => css`
+		position: relative;
+		border-radius: 24px;
 		width: 100%;
-		height: 100%;
-		content: '';
-	}
+		height: 260px;
+		overflow: hidden;
+
+		&::after {
+			position: absolute;
+			top: 0;
+			left: 0;
+			opacity: 0.8;
+			z-index: 1;
+			background: ${theme.tokens['color-base-surface-primary']};
+			width: 100%;
+			height: 100%;
+			content: '';
+		}
+
+		@media (min-width: ${breakpoints.lg}px) {
+			height: 400px;
+
+			&::after {
+				background: linear-gradient(to right, rgb(255 255 255 / 100%), rgb(0 0 0 / 0%));
+			}
+		}
+	`}
 `
 
 export const ImageStyled = styled(Image)`
 	position: absolute;
-	bottom: -90px;
 	border-radius: 24px;
+	min-height: 260px;
 	object-fit: cover;
-	object-position: 50% 80%;
+
+	@media (min-width: ${breakpoints.lg}px) {
+		bottom: -90px;
+	}
 `
 
 export const Title = styled.h1`
 	${({ theme }) => css`
+		${textXlSemibold}
 		color: ${theme.tokens['color-base-content-primary']};
-		${headingXxlBold};
+
+		@media (min-width: ${breakpoints.lg}px) {
+			${headingXxlBold};
+		}
 	`}
 `
 
@@ -53,5 +70,8 @@ export const TextWrapper = styled.div`
 	gap: 8px;
 	z-index: 200;
 	padding: 24px;
-	max-width: 60%;
+
+	@media (min-width: ${breakpoints.lg}px) {
+		max-width: 70%;
+	}
 `
