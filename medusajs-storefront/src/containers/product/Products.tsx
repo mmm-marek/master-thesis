6b88114798e8d43d29aee3fa8fe5f7226912ec39
Marek Matušica <marek.matusica@goodrequest.com>
2024-08-01 11:57:51 +0200

    Add breadcrumbs to products page

--- medusajs-storefront/src/containers/product/Products.tsx
+++ medusajs-storefront/src/containers/product/Products.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
+
+import Breadcrumb from '@/atoms/Breadcrumb/Breadcrumb'
+
 import ProductList from './components/ProductList/ProductList'
 
 const Products = () => {
-	return <ProductList />
+	const t = useTranslations('containers.products')
+
+	return (
+		<>
+			<Breadcrumb
+				items={[
+					{
+						title: <Link href='/'>{t('home')}</Link>
+					},
+					{
+						title: t('products')
+					}
+				]}
+			/>
+			<ProductList />
+		</>
+	)
 }
 
 export default Products
